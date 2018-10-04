8f0e4eb19ee76529eae2e82c7bfa3cfde1a449b3
haomengmeng <2267448785@qq.com>
2018-10-04 21:58:52 +0800

    Home部分

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/views/Login.vue';
+import Home from '@/views/Home.vue';
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    { name: 'login', path: '/login', component: Login }
+    { name: 'login', path: '/login', component: Login },
+    { name: 'home', path: '/', component: Home}
   ]
 });
