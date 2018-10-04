25a269e26d2eaca3cb7b452222536af0d584dcea
haomengmeng <2267448785@qq.com>
2018-10-04 21:11:56 +0800

    登录布局及axios的封装

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/css/index.css';
+import MyHttp from '@/plugins/MyHttp.js';
+Vue.use(MyHttp);
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="login-wrap">
+        <el-form
+        class="login-form"
+        label-position="top"
+        label-width="80px">
+          <h3>用户登录</h3>
+          <el-form-item label="用户名">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input type="password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login-btn" type="primary">登录</el-button>
+          </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style>
+  .login-wrap {
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .login-form {
+    width: 500px;
+    background-color: #fff;
+    padding: 30px;
+    border-radius: 5px;
+  }
+  .login-btn {
+    width: 100%;
+  }
+</style>
